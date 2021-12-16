e8d96e0a4fc9cd3af0c6208a5893daed46c7b737
yaincoding <yaincoding@gmail.com>
2021-12-16 15:57:30 +0900

    rm ES_HOME

--- setup.sh
+++ setup.sh
@@ -4,6 +4,3 @@ export HOST_IP="$(ifconfig eth0 | grep 'inet ' | cut -d: -f2 | awk '{print$2}')"
 
 echo "HOST_IP=${HOST_IP}"
 
-export ES_HOME=$HOME/elasticsearch-7.16.1
-
-echo "ES_HOME=${ES_HOME}"
