46c59e6d30676a7f5b7a001824e29b589441dfcf
yaincoding <yaincoding@gmail.com>
2021-12-16 16:04:14 +0900

    rpm install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+sudo yum install -y java-1.8.0-openjdk-devel.x86_64
+
+sudo touch /etc/yum/repos.d/elasticsearch.repo
+
+echo [elasticsearch-7.x] >> /etc/yum.repos.d/elasticsearch.repo
+echo name=Elasticsearch repository for 7.x packages >> /etc/yum.repos.d/elasticsearch.repo
+echo baseurl=https://artifacts.elastic.co/packages/7.x/yum >> /etc/yum.repos.d/elasticsearch.repo
+echo gpgcheck=1 >> /etc/yum.repos.d/elasticsearch.repo
+echo gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch >> /etc/yum.repos.d/elasticsearch.repo
+echo enabled=1 >> /etc/yum.repos.d/elasticsearch.repo
+echo autorefresh=1 >> /etc/yum.repos.d/elasticsearch.repo
+echo type=rpm-md >> /etc/yum.repos.d/elasticsearch.repo
+
+sudo yum install -y elasticsearch
+
