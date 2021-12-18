1f309b7f44b43f81e950dc23e871838463c886b0
yaincoding <yaincoding>
2021-12-18 20:39:27 +0900

    installation settings write into kibana.repo file

--- kibana/install.sh
+++ kibana/install.sh
@@ -2,14 +2,14 @@
 
 #super user로 실행
 
-[kibana-7.x]
-name=Kibana repository for 7.x packages
-baseurl=https://artifacts.elastic.co/packages/7.x/yum
-gpgcheck=1
-gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
-enabled=1
-autorefresh=1
-type=rpm-md
+echo [kibana-7.x] >> /etc/yum.repos.d/kibana.repo
+echo name=Kibana repository for 7.x packages >> /etc/yum.repos.d/kibana.repo
+echo baseurl=https://artifacts.elastic.co/packages/7.x/yum >> /etc/yum.repos.d/kibana.repo
+echo gpgcheck=1 >> /etc/yum.repos.d/kibana.repo
+echo jgpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch >> /etc/yum.repos.d/kibana.repo
+echo enabled=1 >> /etc/yum.repos.d/kibana.repo
+echo autorefresh=1 >> /etc/yum.repos.d/kibana.repo
+echo type=rpm-md >> /etc/yum.repos.d/kibana.repo
 
 yum install -y kibana-7.16.1
 
