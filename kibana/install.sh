64b13d2208d9cc0a1e1d04cabe532ad7b0e46b6d
yaincoding <yaincoding>
2021-12-18 20:17:56 +0900

    kibana install script

---
+++ kibana/install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+#super user로 실행
+
+[kibana-7.x]
+name=Kibana repository for 7.x packages
+baseurl=https://artifacts.elastic.co/packages/7.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+autorefresh=1
+type=rpm-md
+
+yum install -y kibana-7.16.1
+
