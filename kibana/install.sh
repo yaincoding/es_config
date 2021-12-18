e0493c08ae526c2c84916dd2bf5f3e75addf9f5d
yaincoding <yaincoding>
2021-12-18 20:42:21 +0900

    removed typo

--- kibana/install.sh
+++ kibana/install.sh
@@ -6,7 +6,7 @@ echo [kibana-7.x] >> /etc/yum.repos.d/kibana.repo
 echo name=Kibana repository for 7.x packages >> /etc/yum.repos.d/kibana.repo
 echo baseurl=https://artifacts.elastic.co/packages/7.x/yum >> /etc/yum.repos.d/kibana.repo
 echo gpgcheck=1 >> /etc/yum.repos.d/kibana.repo
-echo jgpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch >> /etc/yum.repos.d/kibana.repo
+echo gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch >> /etc/yum.repos.d/kibana.repo
 echo enabled=1 >> /etc/yum.repos.d/kibana.repo
 echo autorefresh=1 >> /etc/yum.repos.d/kibana.repo
 echo type=rpm-md >> /etc/yum.repos.d/kibana.repo
