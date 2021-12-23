0afe983462296b16dbaf4e6eda698dbbe9dd9f9c
yaincoding <yaincoding@gmail.com>
2021-12-23 10:38:58 +0900

    linux user for kibana change commands added

--- kibana/install.sh
+++ kibana/install.sh
@@ -16,3 +16,14 @@ echo type=rpm-md >> /etc/yum.repos.d/kibana.repo
 
 yum install -y kibana-7.16.1
 
+rm /etc/elasticsearch/elasticsearch.yml
+cp ./$1/elasticsearch.yml /etc/elasticsearch
+
+#elasticsearch 관련 파일 소유권 elasticsearch로 수정
+chown -R kibana:kibana /usr/share/kibana
+chown -R kibana:kibana /var/lib/kibana
+chown -R kibana:kibana /var/log/kibana
+chown -R kibana:kibana /etc/kibana
+
+
+
