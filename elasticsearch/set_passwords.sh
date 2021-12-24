d0351ac8240d02877dfa0034db6b1f92eda2749f
yaincoding <yaincoding@gmail.com>
2021-12-24 15:52:08 +0900

    username and passwords for kibana_system set

--- elasticsearch/set_passwords.sh
+++ elasticsearch/set_passwords.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-curl -X GET -H 'Content-Type: application/json' http://localhost:9200/_security/user/kibana_system/_password -d '{"password": "kibanapassword"}'
+curl -X POST -H 'Content-Type: application/json' http://localhost:9200/_security/user/kibana_system/_password -d '{"password": "kibanapassword"}' -u "elastic":"yacopassword"
 
