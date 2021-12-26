2256193fd479398c5d3be4f38fb5eed87b87be75
yaincoding <yaincoding@gmail.com>
2021-12-26 13:08:16 +0900

    moved passwords setting script file under scripts directory

---
+++ elasticsearch/scripts/set_passwords.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+curl -X POST -H 'Content-Type: application/json' \
+http://localhost:9200/_security/user/kibana_system/_password \
+-d '{"password": "kibanapassword"}' \
+-u "elastic":"yacopassword"
+
