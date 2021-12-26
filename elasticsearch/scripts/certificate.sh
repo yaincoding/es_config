f273518a286e8e0e56badf99f26998a320df90a5
yaincoding <yaincoding@gmail.com>
2021-12-26 16:21:56 +0900

    script for elasticsearch tls/ssl setting

---
+++ elasticsearch/scripts/certificate.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+#CA 생성
+/usr/share/elasticsearch/bin/elasticsearch-certutil ca --out /etc/elasticsearch/elastic-stack-ca.p12 --pass "yacopass"
+
+#certificates 생성
+/usr/share/elasticsearch/bin/elasticsearch-certutil cert \
+--ca "/etc/elasticsearch/elastic-stack-ca.p12" \
+--ca-pass "<ca password>" \
+--out "/etc/elasticsearch/elastic-certificates.p12" \
+--pass "<password>"
+
+#elasticsearch node 접근 가능하도록 권한 elasticsearch 계정으로 변경
+chown -R elasticsearch:elasticsearch /etc/elasticsearch/elastic-stack-ca.p12
+chown -R elasticsearch:elasticsearch /etc/elasticsearch/elastic-certificates.p12
+
+#certificates 패스워드 elasticsearch keystore에 저장
+printf "yacopass" | /usr/share/elasticsearch/bin/elasticsearch-keystore add xpack.security.transport.ssl.keystore.secure_password
+printf "yacopass" | /usr/share/elasticsearch/bin/elasticsearch-keystore add xpack.security.transport.ssl.truststore.secure_password
