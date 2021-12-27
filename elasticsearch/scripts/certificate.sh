7d5d695f3f4540fe2c87c5b99d6f9b6fac1d8a8d
yaincoding <yaincoding@gmail.com>
2021-12-27 10:34:14 +0900

    certificate files path changed to /etc/elasticsearch/certs

--- elasticsearch/scripts/certificate.sh
+++ elasticsearch/scripts/certificate.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/bash
 
 #CA 생성
-/usr/share/elasticsearch/bin/elasticsearch-certutil ca --out /etc/elasticsearch/elastic-stack-ca.p12 --pass "yacopass"
+/usr/share/elasticsearch/bin/elasticsearch-certutil ca --out /etc/elasticsearch/certs/elastic-stack-ca.p12 --pass "yacopass"
 
 #certificates 생성
 /usr/share/elasticsearch/bin/elasticsearch-certutil cert \
---ca "/etc/elasticsearch/elastic-stack-ca.p12" \
+--ca "/etc/elasticsearch/certs/elastic-stack-ca.p12" \
 --ca-pass "yacopass" \
---out "/etc/elasticsearch/elastic-certificates.p12" \
+--out "/etc/elasticsearch/certs/elastic-certificates.p12" \
 --pass "yacopass"
 
 #elasticsearch node 접근 가능하도록 권한 elasticsearch 계정으로 변경
-chown -R elasticsearch:elasticsearch /etc/elasticsearch/elastic-stack-ca.p12
-chown -R elasticsearch:elasticsearch /etc/elasticsearch/elastic-certificates.p12
+chown -R elasticsearch:elasticsearch /etc/elasticsearch/certs
 
 #certificates 패스워드 elasticsearch keystore에 저장
 printf "yacopass" | /usr/share/elasticsearch/bin/elasticsearch-keystore add xpack.security.transport.ssl.keystore.secure_password
